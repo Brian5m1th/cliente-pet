ffbf9e0b03031fbe32065f3a5fc4b43b711daa40
Brian5m1th <briandiscbol@gmail.com>
2025-05-08 19:24:45 -0300

    implementa api de rest

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -2,10 +2,16 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClientePetApplication {
 
+    public String getHomeTest() {
+        return "Cliente Pet - API Home";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ClientePetApplication.class, args);
     }
