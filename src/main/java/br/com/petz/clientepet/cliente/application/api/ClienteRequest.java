9a8099a333720094a9c56e64aea15629f19f779b
Brian5m1th <briandiscbol@gmail.com>
2025-05-08 21:34:24 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteRequest {
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
+
+@Value
+public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    @NotBlank
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    @DateTimeFormat
+    private LocalDateTime dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private boolean aceitaTermos;
 }
